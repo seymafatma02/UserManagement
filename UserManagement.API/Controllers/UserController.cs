1fd9319d8860c72fee9b95f6db71c54c8a5d75de
seymafatma02 <167695027+seymafatma02@users.noreply.github.com>
2024-08-21 23:20:05 +0300

    Test etmek amacıyla get metotu eklendi

---
+++ UserManagement.API/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace UserManagement.API.Controllers
+
+{
+
+    [Route("api/[controller]")]// controllerimin ismi neyse api yanına o gelicek
+
+    public class UserController:ControllerBase // miras aldım 
+    {
+        public string Get() {
+
+            return " Get users";
+                }
+    }
+}
