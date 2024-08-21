8dfc19b2ae1bb246b6e7c7efd43c8eee1955410a
seymafatma02 <167695027+seymafatma02@users.noreply.github.com>
2024-08-21 23:28:35 +0300

    user clasına ozellikler eklendi

---
+++ UserManagement.API/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace UserManagement.API.Models
+{
+    public class User
+    { // user ın ozellikleri
+
+        public  int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+
+
+
+    }
+}
