98a71a3d54fd8831cfc2bb63c3a950708fbbaa14
seymafatma02 <167695027+seymafatma02@users.noreply.github.com>
2024-08-25 23:40:55 +0300

    httppost ,httpput ve httpdelete metotları eklendi

--- UserManagement.API/Controllers/UserController.cs
+++ UserManagement.API/Controllers/UserController.cs
@@ -28,5 +28,31 @@ namespace UserManagement.API.Controllers
             var user =_users.FirstOrDefault(x => x.Id == id);//user bulduruyor ,benim user ın id si sen parametre olarak hangi id gondermissen ona esitle demek 
             return user;
         }
+
+
+        [HttpPost]
+        public User Post([FromBody] User user)
+        {
+            _users.Add(user);//ıd si belli olur
+            return user;//eklediğin useri ıd si ile beraber gonderir
+        }
+
+        [HttpPut]
+        public User Put([FromBody] User user)
+        {
+            var editedUser= _users.FirstOrDefault(x => x.Id==user.Id);
+            editedUser.Firstname=user.Firstname;
+            editedUser.Lastname=user.Lastname;
+            editedUser.Address = user.Address;
+            
+            return user;
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var deleteUser=_users.FirstOrDefault(x => x.Id==id);
+            _users.Remove(deleteUser);
+
+        }
     }
 }
