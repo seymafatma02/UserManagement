c9feeb8df2373fd71cc500b700b2e244e6791e84
seymafatma02 <167695027+seymafatma02@users.noreply.github.com>
2024-08-25 18:15:43 +0300

    id ye gore user etirme metotu eklendi

--- UserManagement.API/Controllers/UserController.cs
+++ UserManagement.API/Controllers/UserController.cs
@@ -11,10 +11,22 @@ namespace UserManagement.API.Controllers
 
     public class UserController:ControllerBase // miras aldım 
     {
-        private List<User> _users = FakeData.GetUsers(200); 
+        private List<User> _users = FakeData.GetUsers(200);
+
+
+
+        [HttpGet]
         public List<User> Get() {
             return _users;
             
+            
                 }
+        [HttpGet("{id}")]
+        public User Get(int id)// 2 .metot ıd ye gore user getirme diger get den farkı ıd diye parametre alması
+        {
+
+            var user =_users.FirstOrDefault(x => x.Id == id);//user bulduruyor ,benim user ın id si sen parametre olarak hangi id gondermissen ona esitle demek 
+            return user;
+        }
     }
 }
