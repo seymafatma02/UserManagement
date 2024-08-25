e341260da192b9b414f602bbf99e47c1f98bd675
seymafatma02 <167695027+seymafatma02@users.noreply.github.com>
2024-08-25 17:27:31 +0300

    fakedataları reflesh olma durumunda kontrol yaptım

--- UserManagement.API/Fake/FakeData.cs
+++ UserManagement.API/Fake/FakeData.cs
@@ -12,12 +12,16 @@ namespace UserManagement.API.Fake
 
         public static List<User> GetUsers(int number)
         {
-            _users = new Faker<User>()
-                .RuleFor(u => u.Id,f =>f.IndexFaker)
-                .RuleFor(u => u.Firstname ,f =>f.Name.FirstName())
-                .RuleFor( u => u.Lastname , f =>f.Name.LastName())  
-                .RuleFor(u => u.Address, f => f.Address.FullAddress())
-                .Generate(number);
+            if(_users == null)
+            {
+                _users = new Faker<User>()
+               .RuleFor(u => u.Id, f => f.IndexFaker + 1)// fake datakardan 1 den baslıcak sekilde ayarlandı
+               .RuleFor(u => u.Firstname, f => f.Name.FirstName())
+               .RuleFor(u => u.Lastname, f => f.Name.LastName())
+               .RuleFor(u => u.Address, f => f.Address.FullAddress())
+               .Generate(number);
+            }
+           
             return _users;
         }
     }
